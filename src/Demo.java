ff71cb2911c0737ad7a5792cbeb32c5d8b8d9833
mynameisnotdave <islandmonkeee@gmail.com>
2023-03-29 12:38:43 +0100

    push

--- src/Demo.java
+++ src/Demo.java
@@ -3,14 +3,12 @@ import java.util.Scanner;
 class Demo{
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        int  number = scanner.nextInt();
-        int fact = 1;
+        double[] revenue = {88750, 125430, 99700, 14500, 158000, 65000, 99000, 189000, 210000, 42000, 165800, 258900};
         //your code goes here
-        while (number > 0) {
-            fact *=number;
-            number--;
+        double average = 0;
+        for (double x: revenue) {
+            average += x;
         }
-        System.out.println(fact);
+        System.out.println(average / revenue.length);
     }
 }
